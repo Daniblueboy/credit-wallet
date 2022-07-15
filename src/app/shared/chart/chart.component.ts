e584435efb2cc18d69155492ca166450f5f272d8
daniblueboy <danielobasuyi@gmail.com>
2022-07-16 00:03:16 +0100

    styled chart component and form

--- src/app/shared/chart/chart.component.ts
+++ src/app/shared/chart/chart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-chart',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartComponent implements OnInit {
 
+  
   constructor() { }
 
   ngOnInit(): void {
+    const myChart = new Chart('myChart', {
+      type: 'doughnut',
+      data: {
+        labels: [
+          'Target Amount - N500,000,000.00',
+          'Loan Disbursed - N105,000,000.00',
+        ],
+        datasets: [
+          {
+            label: '# of Order Completed',
+            data: [12, 19],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+            ],
+            borderColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1)'],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
+
+  
 
 }
